a6d0d993d95e5b8725d11f1b216cd95933ba5d22
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-28 09:53:13 +0200

    Added notification module. Refactored navigation components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { StoreModule } from '@ngrx/store'
-import { NavigationComponent } from './navigation/navigation.component'
-import { LayoutModule } from '@angular/cdk/layout'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatButtonModule } from '@angular/material/button'
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatIconModule } from '@angular/material/icon'
-import { MatListModule } from '@angular/material/list'
 import { CoreRoutingModule } from '@app/core/core-routing.module'
 import { CoreComponent } from './core.component'
-import { ReactiveComponentModule } from '@ngrx/component';
-import { SideNavComponent } from './navigation/side-nav/side-nav.component';
-import { TopNavComponent } from './navigation/top-nav/top-nav.component'
+import { NavigationModule } from '@app/core/navigation/navigation.module'
+import { NotificationsModule } from '@app/core/notifications/notifications.module'
 
 /**
  * The core module that is always loaded after the initial fetch. It consists of the
@@ -20,17 +12,12 @@ import { TopNavComponent } from './navigation/top-nav/top-nav.component'
  * required to included in the root of the app (like NgRx modules).
  */
 @NgModule({
-  declarations: [NavigationComponent, CoreComponent, SideNavComponent, TopNavComponent],
+  declarations: [CoreComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    ReactiveComponentModule,
+    NavigationModule,
+    NotificationsModule,
     StoreModule.forRoot({}),
   ],
 })
--- client/src/app/core/navigation/side-nav/side-nav.component.ts
+++ client/src/app/core/navigation/side-nav/side-nav.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core'
 interface SideNavLink {
   route: string
   label: string
+  disabled: boolean
 }
 
 @Component({
@@ -15,14 +16,22 @@ export class SideNavComponent {
     {
       route: '/dashboard',
       label: 'Dashboard',
+      disabled: false,
+    },
+    {
+      route: '/version-control',
+      label: 'Version Control',
+      disabled: true,
     },
     {
       route: '/settings',
       label: 'Settings',
+      disabled: true,
     },
     {
       route: '/about',
       label: 'About',
+      disabled: true,
     },
   ]
 
