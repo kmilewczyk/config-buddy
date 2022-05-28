8138ce0da04e29f8dcc49ed1e34db408e98f88b4
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-28 18:59:19 +0200

    Added programmable dashboard

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,33 +1,111 @@
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component } from '@angular/core'
+import { map } from 'rxjs/operators'
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout'
+import { filter, Observable, shareReplay } from 'rxjs'
+import { ViewportService } from '@app/core/_services/viewport.service'
+
+interface DashboardItem {
+  id: string
+  title: string
+}
+
+interface DashboardLayout {
+  cols: number
+  items: {
+    id: string
+    cols: number
+    rows: number
+  }[]
+}
+
+interface DashboardItemLayout extends DashboardItem {
+  cols: number
+  rows: number
+}
+
+const DASHBOARD_ITEMS: DashboardItem[] = [
+  { id: 'changes', title: 'Changes' },
+  { id: 'cache_size', title: 'Cache size' },
+  { id: 'cleanup', title: 'Home cleaning' },
+  { id: 'deprecation', title: 'Deprecations' },
+]
+
+const DASHBOARD_LAYOUT: Record<string, DashboardLayout> = {
+  [Breakpoints.HandsetPortrait]: {
+    cols: 1,
+    items: [],
+  },
+  [Breakpoints.HandsetLandscape]: {
+    cols: 2,
+    items: [
+      { id: 'changes', cols: 2, rows: 1 },
+      { id: 'cleanup', cols: 1, rows: 1 },
+    ],
+  },
+  [Breakpoints.TabletPortrait]: {
+    cols: 2,
+    items: [
+      { id: 'changes', cols: 2, rows: 1 },
+      { id: 'cleanup', cols: 1, rows: 2 },
+    ],
+  },
+  [Breakpoints.TabletLandscape]: {
+    cols: 3,
+    items: [
+      { id: 'changes', cols: 2, rows: 1 },
+      { id: 'cleanup', cols: 1, rows: 2 },
+    ],
+  },
+  [Breakpoints.WebPortrait]: {
+    cols: 3,
+    items: [
+      { id: 'changes', cols: 2, rows: 1 },
+      { id: 'cleanup', cols: 1, rows: 2 },
+    ],
+  },
+  [Breakpoints.WebLandscape]: {
+    cols: 3,
+    items: [
+      { id: 'changes', cols: 2, rows: 1 },
+      { id: 'cleanup', cols: 1, rows: 2 },
+    ],
+  },
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+  cols$: Observable<number>
+  cards$: Observable<DashboardItemLayout[]>
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  private layout$: Observable<DashboardLayout>
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private viewport: ViewportService) {
+    this.layout$ = this.viewport.breakpoint$.pipe(
+      filter((layout) => layout !== undefined),
+      map((breakpoint) => DASHBOARD_LAYOUT[breakpoint as string]),
+      shareReplay({ refCount: true, bufferSize: 1 })
+    )
+
+    this.cols$ = this.layout$.pipe(map((layout) => layout.cols))
+
+    this.cards$ = this.layout$.pipe(
+      map((layout) => {
+        return DASHBOARD_ITEMS.map((item) => {
+          const itemLayout = layout.items.find((layout) => layout.id === item.id) ?? {}
+
+          return {
+            ...item,
+            cols: 1,
+            rows: 1,
+            ...itemLayout,
+          }
+        })
+      })
+    )
+  }
 }
