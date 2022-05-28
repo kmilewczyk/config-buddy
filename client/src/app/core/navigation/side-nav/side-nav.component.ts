1dc910cc23cc669b6b82b7a1adee2a3d7cac2553
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-28 08:59:30 +0200

    Added programmable side-nav links

--- client/src/app/core/navigation/side-nav/side-nav.component.ts
+++ client/src/app/core/navigation/side-nav/side-nav.component.ts
@@ -1,10 +1,30 @@
 import { Component, OnInit } from '@angular/core'
 
+interface SideNavLink {
+  route: string
+  label: string
+}
+
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent {
+  links: SideNavLink[] = [
+    {
+      route: '/dashboard',
+      label: 'Dashboard',
+    },
+    {
+      route: '/settings',
+      label: 'Settings',
+    },
+    {
+      route: '/about',
+      label: 'About',
+    },
+  ]
+
   constructor() {}
 }
