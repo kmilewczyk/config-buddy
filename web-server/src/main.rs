f9bbb8cbab5618173860cd4f4b27332cd1618bf1
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-30 18:57:49 +0200

    Added some boilerplate

---
+++ web-server/src/main.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    rocket::build().mount("/", routes![index]).launch().await;
+}
